10694630cabaaa3d8b4d67a18e27cbda1702ec20
Faruk Emre EKŞİOĞLU <faruk.eksioglu@hotmail.com>
2025-02-09 20:09:21 +0300

    fix: 'fetch' was try to zip even if there is no adb device found.
    * if adb fetch fails the program exits.

--- fetch.sh
+++ fetch.sh
@@ -1,5 +1,5 @@
 !#/bin/bash
-adb pull /sdcard/Android/data/com.chucklefish.stardewvalley/files/Saves/
 mkdir archive
+adb pull /sdcard/Android/data/com.chucklefish.stardewvalley/files/Saves/ && \
 zip -r archive/save_`date -Iseconds`.zip Saves/
 
